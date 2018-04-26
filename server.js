776a40eaf527adfd51308764c9e9abf0db9f9042
ivan cai <ivan.z.cai@gmail.com>
2018-04-26 14:29:01 +1000

    created upload api function

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-'use strict';
-
 const express = require('express');
-
-// Constants
+const fileUpload = require('express-fileupload');
+const app = express();
+ // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+// default options
+app.use(fileUpload());
+ 
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+ 
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/Users/ivan/Dropbox/work/TRAINING/docker-nodejs-upload-api/uploadDocs/'+sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 
+    res.send('File uploaded!');
+  });
 });
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
